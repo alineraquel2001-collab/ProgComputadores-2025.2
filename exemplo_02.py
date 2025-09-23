81bc03445b7aaf82383d05c7f72da71942fb85f8
alineraquel2001-colaboração <alineraquel2001@gmail.com>
2025-09-23 20:53:46 -0300

    input

--- exemplo_02.py
+++ exemplo_02.py
@@ -1,10 +1,13 @@
 
-nome = 'Aline';
+#nome = 'Aline';
 
-idade = 24;
-
-#Olá Aline, você tem 24 anos
-print(f'Olá' ,nome, ', você tem' ,idade, 'anos.');
+#idade = 24;
 
 #Olá Aline, você tem 24 anos
-print(f'Olá {nome}, você tem {idade} anos.');
+#print(f'Olá' ,nome, ', você tem' ,idade, 'anos.');
+
+#Olá Aline, você tem 24 anos
+#print(f'Olá {nome}, você tem {idade} anos.');
+
+seunome = input('Digite seu nome:');
+ print(f'olá, {seunome}');
