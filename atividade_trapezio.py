acf5c75c1cac905be1c0be9a2011595e047af877
alineraquel2001-colaboração <alineraquel2001@gmail.com>
2025-09-24 21:21:30 -0300

    atividade do raio

--- atividade_trapezio.py
+++ atividade_trapezio.py
@@ -1,20 +1,22 @@
-#ATIVIDADE - CALCULAR AERA DO TRAPEZIO TRAPEZIO
+#ATIVIDADE - CALCULAR ÁREA DO TRAPÉZIO
 
-print('Vamos calcular a area do trapesio!')
+print('Vamos calcular a área do trapézio!')
 
-#VALOR DA BASE MAIOR
-base_maior = input('Informe o valor da base maior do trapesio:')
+#VALOR DA BASE MAIOR DO TRAPÉZIO
+base_maior = input('Informe o valor da base maior do trapézio:')
 
-#VALOR DA BASE MENOR
-base_menor = input('Informe o valor da base menor do trapesio:')
+#VALOR DA BASE MENOR DO TRAPÉZIO
+base_menor = input('Informe o valor da base menor do trapézio:')
 
-#VALOR DA BASE MAIOR
-altura = input('Informe o valor da altura do trapesio:')
+#VALOR DA ALTURA DO TRAPÉZIO
+altura = input('Informe o valor da altura do trapézio:')
 
 base_maior = float(base_maior)
 base_menor = float(base_menor)
 altura = float(altura)
 
-area = base_maior + base_menor * altura / 2
+#CALCULAR ÁREA
+area = (base_maior + base_menor) * altura / 2
 
-print(f'A area do trapezio é: {area}.')
+#iMPRIMIR O VALOR DA ÁRE DO TRAPÉZIO
+print(f'A Área do trapézio é: {area} .')
